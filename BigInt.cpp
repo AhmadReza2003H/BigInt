73033b84be06c13c9eb245451b7cb184a6b9d7ac
AhmadReza <ahmadreza@DESKTOP-8P7BMBK.localdomain>
2023-09-15 16:55:55 +0430

    second version

--- BigInt.cpp
+++ BigInt.cpp
@@ -3,34 +3,27 @@
 #include <iomanip>
 #include <math.h>
 #include "BigInt.h"
+#define equal 0
+#define greater 1
+#define smaller -1
+#define add 1
+#define sub -1
 std::ostream & operator<<(std::ostream &os , const BigInt &bigInt){
-    // bool isPrinting = false;
-    // for(int i = 4 ; i >= 0 ; i--){
-    //     if(!isPrinting && bigInt.number[i] != 0){
-    //         isPrinting = true;
-    //         os << bigInt.number[i];
-    //     } else if(isPrinting){
-    //     os << std::setfill('0') << std::setw(9) << bigInt.number[i];
-    //     }
-    // }
-    // if(!isPrinting){
-    //     os << 0;
-    // }
-   int firstNoneZero = 4;
-    while (firstNoneZero >= 0 && bigInt.number[firstNoneZero] == 0) {
-        firstNoneZero--;
-    }
-
-    if (firstNoneZero < 0) {
-        os << 0;
-    } else{
-        os << bigInt.number[firstNoneZero];
-        for(int i = firstNoneZero -1 ; i >= 0 ; i--){
-            os << std::setfill('0') << std::setw(9) << bigInt.number[i];
+    bool isPrinting = false;
+    for(int i = 4 ; i >= 0 ; i--){
+        if(!isPrinting && bigInt.number[i] != 0){
+            isPrinting = true;
+            os << bigInt.number[i];
+        } else if(isPrinting){
+        os << std::setfill('0') << std::setw(9) << bigInt.number[i];
         }
+    }
+    if(!isPrinting){
+        os << 0;
     }
     return os;
 }
+
 std::istream & operator>>(std::istream &is , BigInt &bigInt){
     std::string number;
     is >> number;
@@ -41,118 +34,97 @@ std::istream & operator>>(std::istream &is , BigInt &bigInt){
         int startIndex = std::max(endIndex - 9, 0);
         bigInt.number[i] = std::stoi(number.substr(startIndex, endIndex - startIndex));
     }
-    
     return is;
 }
-bool BigInt::operator==(const BigInt &bigInt){
-    for(int i = 0 ; i < 5 ; i++){
-        if(this->number[i] != bigInt.number[i]){
-            return false;
-        }
-    }
-    return true;
+
+bool BigInt::operator==(const BigInt &bigInt) const {
+    return (this->compare(bigInt) == equal);
 }
-bool BigInt::operator!=(const BigInt &bigInt){
-        for(int i = 0 ; i < 5 ; i++){
-        if(this->number[i] != bigInt.number[i]){
-            return true;
-        }
-    }
-    return false;
+
+bool BigInt::operator!=(const BigInt &bigInt) const {
+    return (this->compare(bigInt) != equal);
 }
-bool BigInt::operator<(const BigInt &bigInt){
-    for (int i = 4; i >= 0; i--) {
-        if (this->number[i] < bigInt.number[i]) {
-            return true; 
-        } else if (this->number[i] > bigInt.number[i]) {
-            return false; 
-        }
-    }
-    return false; 
+
+bool BigInt::operator<(const BigInt &bigInt) const {
+    return (this->compare(bigInt) == smaller);
 }
-bool BigInt::operator<=(const BigInt &bigInt){
-    for (int i = 4; i >= 0; i--) {
-        if(this->number[i] == bigInt.number[i]){
-            continue;
-        } else if (this->number[i] < bigInt.number[i]) {
-            return true; 
-        } else {
-            return false; 
-        }
-    }
-    return true;
+
+bool BigInt::operator<=(const BigInt &bigInt) const {
+    int compare = this->compare(bigInt);
+    return (compare == equal || compare == smaller);
 }
-bool BigInt::operator>(const BigInt &bigInt){
-    for (int i = 4; i >= 0; i--) {
-        if (this->number[i] > bigInt.number[i]) {
-            return true; 
-        } else if (this->number[i] < bigInt.number[i]) {
-            return false; 
-        }
-    }
-    return true; 
+
+bool BigInt::operator>(const BigInt &bigInt)const { 
+    return (this->compare(bigInt) == greater);
 }
-bool BigInt::operator>=(const BigInt &bigInt){
-        for (int i = 4; i >= 0; i--) {
-        if(this->number[i] == bigInt.number[i]){
-            continue;
-        } else if (this->number[i] > bigInt.number[i]) {
-            return true; 
-        } else {
-            return false; 
-        }
-    }
-    return true;
+
+bool BigInt::operator>=(const BigInt &bigInt) const {
+    int compare = this->compare(bigInt);
+    return (compare == equal || compare == greater);
 }
-BigInt BigInt::operator+(const BigInt &bigInt){
+
+BigInt BigInt::operator+(const BigInt &bigInt) {
+    return this->plusMinusOps(bigInt , add);
+}
+
+BigInt BigInt::operator-(const BigInt &bigInt) {
+    return this->plusMinusOps(bigInt , sub);
+}
+
+BigInt BigInt::plusMinusOps(const BigInt &bigInt , int operation){
     BigInt returnBigInt;
     for(int i = 0 ; i < 5 ; i++){
-        returnBigInt.number[i] += this->number[i] + bigInt.number[i];
-        if(returnBigInt.number[i] >= pow(10 , 9)){
+        returnBigInt.number[i] += this->number[i] + (bigInt.number[i] * operation );
+        if(returnBigInt.number[i] < 0 || returnBigInt.number[i] >= pow(10 , 9)){
             if(i == 4){
                 throw "out of range";
             }
-            returnBigInt.number[i+1]++;
-            returnBigInt.number[i] -= pow(10,9);
+            returnBigInt.number[i+1] += operation;
+            returnBigInt.number[i] += pow(10,9)*(operation*(-1));
         } 
     }
     return returnBigInt;
 }
-BigInt BigInt::operator-(const BigInt &bigInt){
-    BigInt returnBigInt;
-    for(int i = 0 ; i < 5 ; i++){
-        returnBigInt.number[i] += this->number[i] - bigInt.number[i];
-        if(returnBigInt.number[i] < 0 ){
-            if(i == 4){
-                throw "out of range";
-            }
-            returnBigInt.number[i+1]--;
-            returnBigInt.number[i] += pow(10,9);
-        } 
+
+int BigInt::compare(const BigInt & bigInt) const {
+    for (int i = 4; i >= 0; i--) {
+        if(this->number[i] == bigInt.number[i]){
+            continue;
+        } else if (this->number[i] > bigInt.number[i]) {
+            return greater; 
+        } else {
+            return smaller; 
+        }
     }
-    return returnBigInt;
+    return equal;
 }
+
 BigInt::BigInt(){
     
 }
+
 BigInt::BigInt(int x0){
     this->number[0] = x0;
 }
+
 BigInt::BigInt(int x0 , int x1){
     this->number[0] = x0;
     this->number[1] = x1;
 }
+
 BigInt::BigInt(int x0 , int x1 , int x2){
     this->number[0] = x0;
     this->number[1] = x1;
     this->number[2] = x2;
 }
+
 BigInt::BigInt(int x0 , int x1 , int x2 , int x3){
     this->number[0] = x0;
     this->number[1] = x1;
     this->number[2] = x2;
     this->number[3] = x3;
 }
+
 BigInt::BigInt(int x0 , int x1 , int x2 , int x3 , int x4){
     this->number[0] = x0;
     this->number[1] = x1;
--- BigInt.h
+++ BigInt.h
@@ -1,7 +1,7 @@
 #ifndef _BIGINT_H
 #define _BIGINT_H
 #include <iostream>
-#define null -1
+
 class BigInt{
 public:
     BigInt();
@@ -12,18 +12,20 @@ public:
     BigInt(int x0 , int x1 , int x2 , int x3 , int x4);
     BigInt operator+(const BigInt &bigInt);
     BigInt operator-(const BigInt &bigInt);
-    bool operator<(const BigInt &bigInt);
-    bool operator<=(const BigInt &bigInt);
-    bool operator>(const BigInt &bigInt);
-    bool operator>=(const BigInt &bigInt);
-    bool operator==(const BigInt &bigInt);
-    bool operator!=(const BigInt &bigInt);
+    bool operator<(const BigInt &bigInt) const;
+    bool operator<=(const BigInt &bigInt) const;
+    bool operator>(const BigInt &bigInt) const;
+    bool operator>=(const BigInt &bigInt) const;
+    bool operator==(const BigInt &bigInt) const;
+    bool operator!=(const BigInt &bigInt) const;
 
     friend std::ostream & operator<<(std::ostream &os , const BigInt &bigInt);
     friend std::istream & operator>>(std::istream &is , BigInt &bigInt);
 
 private:
     int number[5] = {0 , 0 , 0 , 0 , 0};
+    int compare(const BigInt &bigInt) const;
+    BigInt plusMinusOps(const BigInt & bigInt , int operation);
 };
 
 std::istream & operator>>(std::istream &is , const BigInt &bigInt);
